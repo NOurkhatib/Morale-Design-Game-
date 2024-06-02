da50b2d00f4f9ba48b5872c1f9f716ef48648265
NOurkhatib <m.n.khatib2016@gmail.com>
2024-06-02 15:46:01 +0200

    working on argumnt db

---
+++ backend_nodejs/db/argumnet_db.js
@@ -0,0 +1,20 @@
+
+// const { Pool } = require('pg');
+
+// // Create a new pool instance to manage connections to the PostgreSQL database
+// const pool = new Pool({
+//   user: 'postgres',          // Database user
+//   host: 'localhost',         // Database host
+//   database: 'mydatabase',    // Name of the database
+//   password: 'yourpassword',  // Password for the database user
+//   port: 5432,                // Port number (default is 5432)
+// });
+
+// // Function to execute a query on the database
+// const query = (text, params) => {
+//   return pool.query(text, params);
+// };
+
+// module.exports = {
+//   query,
+// };
