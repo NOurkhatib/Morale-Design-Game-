2153502609d562baf3300f196f930c14bf620ecd
luay-pc <shataraluay@gmail.com>
2024-06-03 12:49:33 +0200

    added a from for to test post requests for back-end

--- md-front-end/app/routes/_index.jsx
+++ md-front-end/app/routes/_index.jsx
@@ -1,17 +1,54 @@
-import SVGBoard from "./svgboard";
+import React, { useState } from 'react';
+import SVGBoard from './svgboard';
+import AddArgumentsForm from './addArgumentsForm';
+
 
 export const meta = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
+  const [value, setValue] = useState('');
+
+  const handleClickSlice = (event, sliceId) => {
+    event.preventDefault();
+    setValue(sliceId);
+  };
+
   return (
-    <div>
-      <SVGBoard/>
- 
+    <div style={styles.container}>
+      <div style={styles.formContainer}>
+        <AddArgumentsForm value={value} setValue={setValue} />
+      </div>
+      <div style={styles.boardContainer}>
+        <SVGBoard onClickSlice={handleClickSlice} />
+      </div>
     </div>
   );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100vh',
+  },
+  formContainer: {
+    flex: '0 0 25%', // Take up 1/4 of the screen
+    padding: '20px',
+    boxSizing: 'border-box',
+    backgroundColor: 'purple',
+    color: 'white',
+    height: '100%',
+  },
+  boardContainer: {
+    flex: '1', // Take up the remaining space
+    padding: '20px',
+    boxSizing: 'border-box',
+    height: '100%',
+    
+  },
+};
