21574dcf7ea1cc9fa3f51b98c5fa113d6024113b
NOurkhatib <m.n.khatib2016@gmail.com>
2024-06-12 17:43:15 +0200

    after downloading node modules to see if there are upluaded or not

--- backend_nodejs/middlewares/authMiddleware.js
+++ backend_nodejs/middlewares/authMiddleware.js
@@ -1,2 +1,4 @@
 // Contains middleware functions for processing requests before they reach the controllers.
 // authMiddleware.js: Example middleware for authentication.
+
+// this is to add external services 
